47017a7820d50f896a95ae00ad372765e687fd58
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2023-05-17 21:26:15 +0800

    Allow unzipper to move sub folders into the main folder

--- utils/unzip.py
+++ utils/unzip.py
@@ -1,4 +1,6 @@
 import os
+import glob
+import shutil
 import zipfile
 
 class ZipHandler:
@@ -6,14 +8,30 @@ class ZipHandler:
         self.path = path
     
     def unzip_all(self) -> None:
-        for file in os.listdir(self.path):
-            if file.endswith(".zip"):
-                with zipfile.ZipFile(file, "r") as zip_ref:
-                    zip_ref.extractall(self.path)
+        for file in glob.glob(self.path + "*.zip"):
+            with zipfile.ZipFile(file, "r") as zip_ref:
+                zip_ref.extractall(self.path + file[:-4])
+            # check if folder has only one folder in it
+            # if so, move all files from that folder to the parent folder
+            # and delete the now empty folder
+            folder = os.listdir(self.path + file[:-4])
+            if len(folder) == 1:
+                for f in os.listdir(self.path + file[:-4] + "/" + folder[0]):
+                    shutil.move(self.path + file[:-4] + "/" + folder[0] + "/" + f, self.path + file[:-4])
+                os.rmdir(self.path + file[:-4] + "/" + folder[0])
+            # delete the zip file
+            os.remove(file)
     
     def unzip(self, file: str):
         with zipfile.ZipFile(file, "r") as zip_ref:
-            zip_ref.extractall(self.path)
+            zip_ref.extractall(self.path + file[:-4])
+            folder = os.listdir(self.path + file[:-4])
+            if len(folder) == 1:
+                for f in os.listdir(self.path + file[:-4] + "/" + folder[0]):
+                    shutil.move(self.path + file[:-4] + "/" + folder[0] + "/" + f, self.path + file[:-4])
+                os.rmdir(self.path + file[:-4] + "/" + folder[0])
+            # delete the zip file
+            os.remove(file)
 
 if __name__ == '__main__':
     zip_handler = ZipHandler()
