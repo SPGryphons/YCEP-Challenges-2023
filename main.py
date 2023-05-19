af6835b16ef617240e5d882a52c8be52a05c3895
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2023-05-19 15:56:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from utils.unzip import ZipHandler
 from utils.sorter import Sorter
 from utils.find import Finder
 from utils.repair import Repair
+from utils.check_csv import check_csv
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--sort", help="Sort all challenges into their respective categories", action="store_true")
@@ -12,6 +13,7 @@ parser.add_argument("-c", "--count", help="Count number of challenges", action="
 parser.add_argument("-cA", "--diffs", help="Count number of challenges by difficulty and total", action="store_true")
 parser.add_argument("-f", "--find", help="Find a challenge")
 parser.add_argument("-r", "--repair", help="Attempt to repair a challenge, may not work all the time")
+parser.add_argument("-C", "--check-csv", help="Check a csv file for challenges")
 
 args = parser.parse_args()
 
@@ -47,6 +49,9 @@ if __name__ == '__main__':
         except Exception as e:
             print(e.args)
             print("Unable to repair challenge.")
+    
+    elif args.check_csv:
+        check_csv(args.check_csv)
     
     else:
         print("No arguments passed. Use -h or --help for help.")
